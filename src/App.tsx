9be20e5def29bea9e727e30c9a29ef31fe83d3de
Abderrahmane bnd <abdoubendaia7@gmail.com>
2024-12-12 09:46:01 +0100

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <div>
+      <Button className="text-lg bg-green-400">Click me</Button>
+    </div>
+  );
+}
