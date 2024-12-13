7630fc405dc60ce23c114cf72256a332f4a1b89b
Abderrahmane bnd <abdoubendaia7@gmail.com>
2024-12-13 14:55:24 +0100

    feat:implementing the contact form

--- src/ui/CustomButton.tsx
+++ src/ui/CustomButton.tsx
@@ -1,16 +1,22 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { cn } from "@/lib/utils";
 
 export default function CustomButton({
   children,
   className,
+  type = "button",
 }: {
   children: React.ReactNode;
   className?: string;
+  type?: "button" | "submit" | "reset";
 }) {
   return (
     <Button
-      className={`border-slate-3 text-lg py-5 px-3 hover:bg-main hover:bg-opacity-90 hover:text-white transition-colors duration-200 text-black border-stroke border shadow-sm rounded-xl ${className}`}
+      type={type}
+      className={cn(
+        `border-slate-3 text-lg py-5 px-3 hover:bg-main hover:bg-opacity-90 hover:text-white transition-colors duration-200 text-black border-stroke border shadow-sm rounded-xl`,
+        className
+      )}
     >
       {children}
     </Button>
--- src/ui/CustomTitle.tsx
+++ src/ui/CustomTitle.tsx
@@ -4,7 +4,7 @@ export default function CustomTitle({
   children: React.ReactNode;
 }) {
   return (
-    <h1 className="text-3xl font-bold text-center mb-7 text-main">
+    <h1 className="text-2xl sm:text-3xl font-bold text-center mb-7 text-main">
       {children}
     </h1>
   );
