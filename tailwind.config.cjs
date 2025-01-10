410aced3c732734052874dd4894313aad0b59913
Abderrahmane bnd <abdoubendaia7@gmail.com>
2025-01-10 15:33:31 +0100

    feat: add ThemeProvider for managing light and dark themes with system preference support

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false,
+  darkMode: "media",
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {
